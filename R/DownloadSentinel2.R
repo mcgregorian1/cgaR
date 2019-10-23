d901d2fa45a9ec33fd9a6aec089b058e444d0129
Ian McGregor <imcgreg@ncsu.edu>
2019-10-23 15:20:58 -0400

    Fixed quick typo

--- R/DownloadSentinel2.R
+++ R/DownloadSentinel2.R
@@ -12,7 +12,7 @@
 #' @param tilename what tile do you want to download
 #' @param folder local folder used to store downloaded files
 #' @param L2Flag indicate to download L2A product or L1C product (default)
-#' @param regExp regular expression, with which you can speicify which files you want. For example,
+#' @param regExp regular expression, with which you can specify which files you want. For example,
 #' regExp <- "S2\\D_MSIL\\d\\D_201([6]|[8][0][0-9]).*.SAFE/$" means you want data from 2016 and before 2018 before October.
 #' @keywords sentinel
 #' @export
